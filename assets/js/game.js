8cbe992fd55e5204c52b7723ebc4124315f80d01
Matt Martin <matt@enigma.local>
2020-01-19 14:36:29 -0500

    firebase cleanup

---
+++ assets/js/game.js
@@ -0,0 +1,27 @@
+// Game variable to hold wins and losses
+let game = {
+    wins: 0,
+    losses: 0,
+    tie: 0
+};
+
+
+
+let gameID = "";
+
+$("#new-game").on("click", function(){
+    // Create a new unique ID. 
+    // Assign it in the database
+    // Show the game ID on the page
+    // reset the game
+});
+
+$("#resume-game").on("click", function(){
+    // set game ID to the inputted ID
+    // Pull variables out of the DB and store locally
+
+});
+
+$(document).ready(function(){
+
+});
