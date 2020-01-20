c8043bf2d45658d727ad3510050296e1e3f377d6
Matt Martin <matt@enigma.local>
2020-01-19 19:25:24 -0500

    game logic working for single screen; displays score

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,12 +2,49 @@
 let game = {
     wins: 0,
     losses: 0,
-    tie: 0
+    tie: 0, 
+    playerHasSelected: false,
+    opponentHasSelected: false,
+    playerSelection: "",
+    opponenentSelection: ""
 };
 
-
-
 let gameID = "";
+
+let timer;
+
+// Function: compare
+function compare(){
+    if(game.playerSelection === game.opponenentSelection){
+        return "tie";
+    } else if(game.playerSelection === "rock" && game.opponenentSelection === "paper"){
+        return "lose";
+    } else if(game.playerSelection === "rock" && game.opponenentSelection === "scissor"){
+        return "win";
+    } else if(game.playerSelection === "scissor" && game.opponenentSelection === "paper"){
+        return "win";
+    } else if(game.playerSelection === "scissor" && game.opponenentSelection === "rock"){
+        return "lose"
+    } else if(game.playerSelection === "paper" && game.opponenentSelection === "rock"){
+        return "win"
+    } else if(game.playerSelection === "paper" && game.opponenentSelection === "scissor"){
+        return "lose"
+    }
+};
+
+function displayScore(){
+    $("#score").attr("class", "fixed");
+    $("#score-wins").text("Wins: " + game.wins);
+    $("#score-losses").text("Losses: " + game.losses);
+    $("#score-tie").text("Wins: " + game.tie);
+}
+
+function resetGame(){
+    clearTimeout();
+    $("#game-display").html("<h1 class='display-4'>Make a Selection</h1>");
+    $("#your-selection").text("");
+    $("#opponent-selection").text("");
+}
 
 $("#new-game").on("click", function(){
     // Create a new unique ID. 
@@ -16,13 +53,54 @@ $("#new-game").on("click", function(){
     // Show the game ID on the page
     // reset the game
     // clear the page
-    $("#game-display").html("");
+    resetGame();
+    displayScore();
 });
 
 $("#resume-game").on("click", function(){
     // set game ID to the inputted ID
     // Pull variables out of the DB and store locally
 
+});
+
+$(document).on("click", ".selection-btn", function(){
+    if(!game.playerHasSelected){
+        game.playerHasSelected = true;
+        game.playerSelection = $(this).attr("data-type");
+    
+        // Display to your selection spot
+        $("#your-selection").text(game.playerSelection);
+        $("#game-display").html("<h1 class='display-4'>Oppenent, your turn.</h1>");
+    } else if (game.playerHasSelected && !game.opponentHasSelected){
+        // move to the opponent
+        game.opponentHasSelected = true;
+        game.opponenentSelection = $(this).attr("data-type");
+    
+        // Display to your selection spot
+        $("#opponent-selection").text(game.opponenentSelection);
+        winner = compare();
+        if(winner === "win"){
+            // Update Score
+            game.wins++;
+            // Show winnerz
+            $("#game-display").html("<h1 class='display-4'>you win</h1>");
+        } else if(winner === "lose"){
+            // Update Score
+            game.losses++;
+            // Show winnerz
+            $("#game-display").html("<h1 class='display-4'>you lose</h1>");
+        } else if(winner === "tie"){
+            // Update Score
+            game.tie++;
+            // Show winnerz
+            $("#game-display").html("<h1 class='display-4'>you tie</h1>");
+        }
+        game.playerHasSelected = false;
+        game.opponentHasSelected = false;
+        displayScore();
+        timer = setTimeout(resetGame, 3000);
+    }
+    
 });
 
 $(document).ready(function(){
